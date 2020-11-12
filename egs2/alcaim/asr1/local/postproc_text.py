5fd01f17484495bbc816ac1736be45713268ef7c
Andre Schlichting <andrelucas@linse.ufsc.br>
2020-11-12 13:59:21 -0300

    Update text process

--- egs2/alcaim/asr1/local/postproc_text.py
+++ egs2/alcaim/asr1/local/postproc_text.py
@@ -4,7 +4,7 @@ import shutil
 from tempfile import mkstemp
 
 def process_text(text: str) -> str:
-    return text.lower().replace(',','').replace('.','').replace('-','').replace('?','').replace('!','').replace(':','').replace('"','')
+    return text.lower().replace(',','').replace('.','').replace('-','').replace('?','').replace('!','').replace(':','').replace('"','').replace("'","")
 
 def main(text_file: str) -> None:
     if not os.path.exists(text_file):
