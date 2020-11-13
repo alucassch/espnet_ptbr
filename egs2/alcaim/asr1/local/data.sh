d82ef41a0679d418910c28f4271ec63d33a59ee3
Andre Schlichting <andrelucas@linse.ufsc.br>
2020-11-13 11:39:09 -0300

    Remove Voxforge data prep of igorq

--- egs2/alcaim/asr1/local/data.sh
+++ egs2/alcaim/asr1/local/data.sh
@@ -11,12 +11,12 @@ log() {
 }
 SECONDS=0
 
+force=false
+
 stage=1
 stop_stage=100000
 data_url=http://www02.smt.ufrj.br/~igor.quintanilha
 data_url_cv=https://cdn.commonvoice.mozilla.org/cv-corpus-5.1-2020-06-22
-train_set=train
-train_dev=dev
 recog_set="lapsbm-val lapsbm-test voxforge-ptbr"
 
 log "$0 $*"
@@ -47,15 +47,15 @@ if [ -z "${VOXFORGE}" ]; then
 fi
 
 if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
-	echo "stage 1: Data Download to ${ALCAIM}"
-    for part in lapsbm-val lapsbm-test voxforge-ptbr alcaim sid; do
+	echo "stage 1.1: Data Download to ${ALCAIM}"
+    for part in lapsbm-val lapsbm-test alcaim sid; do
             local/download_and_untar.sh ${ALCAIM} ${data_url} ${part}
 	done
-    echo "stage 1: Data Download to ${COMMONVOICE}"
+    echo "stage 1.1: Data Download to ${COMMONVOICE}"
     local/download_and_untar.sh ${COMMONVOICE} ${data_url_cv} pt
 
     if [ ! -e "${VOXFORGE}/voxforge/pt/extracted" ]; then
-        echo "stage 1: Data Download to ${VOXFORGE}"
+        echo "stage 1.1: Data Download to ${VOXFORGE}"
         local/getdata.sh pt ${VOXFORGE}/voxforge
     else
         log "stage 1: ${VOXFORGE}/voxforge/pt/extracted is already existing. Skip data downloading"
@@ -64,7 +64,7 @@ fi
 
 
 if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
-    log "stage 2: Preparing data for Alcaim, LapsBM & Sid"
+    log "stage 1.2: Preparing data for Alcaim, LapsBM & Sid"
     local/data_prep_lapsbm.sh ${ALCAIM}/lapsbm-val data/lapsbm-val
     local/data_prep_lapsbm.sh ${ALCAIM}/lapsbm-test data/lapsbm-test
     local/data_prep_sid.sh ${ALCAIM}/sid data/sid
@@ -73,7 +73,7 @@ fi
 
 
 if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then
-    log "stage 3: Preparing data for commonvoice"
+    log "stage 1.3: Preparing data for commonvoice"
     ### Task dependent. You have to make data the following preparation part by yourself.
     for part in "validated" "test" "dev"; do
         # use underscore-separated names in data directories.
@@ -97,18 +97,17 @@ if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then
     utils/fix_data_dir.sh data/${train_set_cv}
 
     utils/combine_data.sh --extra_files utt2num_frames data/commonvoice-ptbr data/${train_set_cv} data/${train_dev_cv} data/${train_dev_cv}
-
 fi
 
 
 if [ ${stage} -le 4 ] && [ ${stop_stage} -ge 4 ]; then
-    log "stage 4: Preparing data for voxforge"
+    log "stage 1.4: Preparing data for voxforge"
     selected=${VOXFORGE}/voxforge/pt/extracted
     # Initial normalization of the data
     local/voxforge_data_prep.sh --flac2wav true "${selected}" "pt"
     local/voxforge_format_data.sh "pt"
 
-    log "stage 4: Split all_pt into data/voxforge_tr_pt data/voxforge_dt_pt data/voxforge_et_pt"
+    log "stage 1: Split all_pt into data/voxforge_tr_pt data/voxforge_dt_pt data/voxforge_et_pt"
     # following split consider prompt duplication (but does not consider speaker overlap instead)
     local/split_tr_dt_et.sh data/all_pt data/voxforge_tr_pt data/voxforge_dt_pt data/voxforge_et_pt
     rm -rf data/all_pt data/local
@@ -121,15 +120,12 @@ if [ ${stage} -le 4 ] && [ ${stop_stage} -ge 4 ]; then
     utils/fix_data_dir.sh data/voxforge_dt_pt
     utils/fix_data_dir.sh data/voxforge_et_pt
 
-    utils/combine_data.sh --extra_files utt2num_frames data/voxforge-ptbr data/voxforge_tr_pt data/voxforge_dt_pt data/voxforge_et_pt
+    utils/combine_data.sh --extra_files utt2num_frames data/voxforge data/voxforge_tr_pt data/voxforge_dt_pt data/voxforge_et_pt
     rm -rf exp/data_prep_pt
 fi
 
-
 if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
-    log "stage 5: combine all training and development sets"
-    utils/combine_data.sh --extra_files utt2num_frames data/${train_set} data/alcaim
-    utils/combine_data.sh --extra_files utt2num_frames data/${train_dev} data/sid
+    log "stage 1.5: combine training development and test sets"
     utils/combine_data.sh --extra_files utt2num_frames data/lapsbm data/lapsbm-val data/lapsbm-test
 fi
 
