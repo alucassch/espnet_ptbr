4d1d47cc2ce4bdd466771c044ef9cb9f378c5430
Andre Schlichting <andrelucas@linse.ufsc.br>
2020-11-22 11:25:38 -0300

    add specaug

--- egs2/alcaim/asr1/run_projeto.sh
+++ egs2/alcaim/asr1/run_projeto.sh
@@ -5,44 +5,47 @@ set -e
 set -u
 set -o pipefail
 
-datasets="voxforge commonvoice"
+datasets="commonvoice voxforge"
 model_types="rnn vggrnn transformer"
 feature_types="raw fbank_pitch"
 token_types="char bpe"
 
 inference_config=conf/decode_asr.yaml
 
+stage=1
+
 for dataset in ${datasets}; do
-    for model_type in ${model_types}; do
+    for feature_type in ${feature_types}; do
+        for model_type in ${model_types}; do
     
-        if [ "${dataset}" = voxforge ]; then
-            train_set="voxforge_tr_pt"
-            valid_set="voxforge_dt_pt"
-            test_sets="voxforge_et_pt lapsbm commonvoice-ptbr sid"
-        elif [ "${dataset}" = commonvoice ]; then
-            train_set="commonvoice_train_pt"
-            valid_set="commonvoice_dev_pt"
-            test_sets="commonvoice_test_pt lapsbm voxforge sid"
-        fi
+            if [ "${dataset}" = voxforge ]; then
+                train_set="voxforge_tr_pt"
+                valid_set="voxforge_dt_pt"
+                test_sets="voxforge_et_pt lapsbm commonvoice-ptbr sid"
+            elif [ "${dataset}" = commonvoice ]; then
+                train_set="commonvoice_train_pt"
+                valid_set="commonvoice_dev_pt"
+                test_sets="commonvoice_test_pt lapsbm voxforge sid"
+            fi
 
-        if [ "${model_type}" = vggrnn ]; then
-            asr_config=conf/train_testes/train_asr_vggrnn_${dataset}.yaml
-        elif [ "${model_type}" = rnn ]; then
-            asr_config=conf/train_testes/train_asr_rnn_${dataset}.yaml
-        elif [ "${model_type}" = transformer ]; then
-            asr_config=conf/train_testes/train_asr_transformer_${dataset}.yaml
-        fi
+            if [ "${model_type}" = vggrnn ]; then
+                asr_config=conf/train_testes/train_asr_vggrnn_${dataset}.yaml
+            elif [ "${model_type}" = rnn ]; then
+                asr_config=conf/train_testes/train_asr_rnn_${dataset}.yaml
+            elif [ "${model_type}" = transformer ]; then
+                asr_config=conf/train_testes/train_asr_transformer_${dataset}.yaml
+            fi
 
-        for feature_type in ${feature_types}; do
+
             for token_type in ${token_types}; do
                 asr_tag="train_${dataset}_${model_type}_${feature_type}_${token_type}"
                 echo "$asr_tag" > wip.txt
                 if [ ! -f "${asr_tag}.done" ]; then   
                     ./asr.sh \
-                        --stage 1 \
+                        --stage "${stage}" \
                         --asr_tag "${asr_tag}" \
-                        --nj 250 \
-                        --inference_nj 250 \
+                        --nj 400 \
+                        --inference_nj 300 \
                         --lang pt \
                         --ngpu 2 \
                         --feats_type ${feature_type} \
@@ -58,11 +61,10 @@ for dataset in ${datasets}; do
                         --valid_set "${valid_set}" \
                         --test_sets "${test_sets}" \
                         --srctexts "data/${train_set}/text" "$@"
-                    
                     touch "${asr_tag}".done
                 else
-		    echo "skip ${asr_tag}"
-		fi
+                    echo "skip ${asr_tag}"
+                fi
             done
         done
     done
