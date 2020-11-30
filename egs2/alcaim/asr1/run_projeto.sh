abac8b44f5e63fcc814a78f90469fd8eccc41128
Andre Schlichting <andrelucas@linse.ufsc.br>
2020-11-30 16:02:35 -0300

    Mantem apenas o experimento com o commonvoice como dataset de treinamento

--- egs2/alcaim/asr1/run_projeto.sh
+++ egs2/alcaim/asr1/run_projeto.sh
@@ -5,9 +5,9 @@ set -e
 set -u
 set -o pipefail
 
-datasets="commonvoice voxforge"
+datasets="commonvoice"
 model_types="rnn vggrnn transformer"
-feature_types="raw fbank_pitch"
+feature_types="raw"
 token_types="char bpe"
 
 inference_config=conf/decode_asr.yaml
@@ -18,15 +18,9 @@ for dataset in ${datasets}; do
     for feature_type in ${feature_types}; do
         for model_type in ${model_types}; do
     
-            if [ "${dataset}" = voxforge ]; then
-                train_set="voxforge_tr_pt"
-                valid_set="voxforge_dt_pt"
-                test_sets="voxforge_et_pt lapsbm commonvoice-ptbr sid"
-            elif [ "${dataset}" = commonvoice ]; then
-                train_set="commonvoice_train_pt"
-                valid_set="commonvoice_dev_pt"
-                test_sets="commonvoice_test_pt lapsbm voxforge sid"
-            fi
+            train_set="commonvoice_train_pt"
+            valid_set="commonvoice_dev_pt"
+            test_sets="commonvoice_test_pt lapsbm voxforge sid"
 
             if [ "${model_type}" = vggrnn ]; then
                 asr_config=conf/train_testes/train_asr_vggrnn_${dataset}.yaml
